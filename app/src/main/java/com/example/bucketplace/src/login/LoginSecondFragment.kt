e8e6dfd8bd1265c800cc3630f62c310b84560b6f
joye-seo <104261048+joye-seo@users.noreply.github.com>
2022-07-01 05:48:37 +0900

    [Main] 홈, 스토어, 마이페이지 뼈대작업 완료

---
+++ app/src/main/java/com/example/bucketplace/src/login/LoginSecondFragment.kt
@@ -0,0 +1,14 @@
+package com.example.bucketplace.src.login
+
+import android.os.Bundle
+import android.view.View
+import com.example.bucketplace.R
+import com.example.bucketplace.config.BaseFragment
+import com.example.bucketplace.databinding.FragmentLoginSecondBinding
+import com.example.bucketplace.databinding.FragmentMainHomeBinding
+
+class LoginSecondFragment : BaseFragment<FragmentLoginSecondBinding>(FragmentLoginSecondBinding::bind, R.layout.fragment_main_home){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/bucketplace/src/splash/SplashSecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bucketplace.src.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bucketplace.R
+
+class SplashSecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_second)
+    }
+}
